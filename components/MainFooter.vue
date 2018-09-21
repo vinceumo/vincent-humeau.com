ffac2eb82ce4c647ec714f477d7854d7bf1c9801
Vincent Humeau <hello@vincent-humeau.com>
2018-09-21 21:55:12 +0100

    Started to create nav and footer

---
+++ components/MainFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="has-bg-dark">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum dolor similique suscipit fugiat dolores? Tempore hic ipsum numquam nam ut. Quibusdam, beatae assumenda? Repudiandae sunt explicabo natus nisi recusandae!</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "MainFooter"
+}
+</script>
---
+++ components/MainNavigation.vue
@@ -0,0 +1,15 @@
+<template>
+<nav>
+  <ul>
+    <li><nuxt-link to="/">Home page</nuxt-link></li>
+    <li><nuxt-link to="/articles">Articles</nuxt-link></li>
+    <li><nuxt-link to="/projects">Projects</nuxt-link></li>
+  </ul>
+</nav>
+</template>
+
+<script>
+export default {
+  name: "MainNavigation"
+}
+</script>
