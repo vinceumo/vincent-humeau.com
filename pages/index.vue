1eccafa94b40256c45fea5965c6cbd77043fcb6c
Vincent Humeau <hello@vincent-humeau.com>
2018-09-24 04:53:28 +0100

    Removed default nuxt content

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,8 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        vincent-humeau
-      </h1>
-      <h2 class="subtitle">
-        My luminous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="btn is-dark">GitHub</a>
-      </div>
-    </div>
+    <h1>Hello world</h1>
   </section>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
 
 <style lang="scss">
 
