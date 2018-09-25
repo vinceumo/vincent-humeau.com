0063b5dd4b055e654386b1eda01af74a6354a815
Vincent Humeau <hello@vincent-humeau.com>
2018-09-25 09:17:14 +0100

    Update navigation

--- components/MainNavigationList.vue
+++ components/MainNavigationList.vue
@@ -1,7 +1,7 @@
 <template>
   <ul>
     <li
-      v-for="(item, index) in items"
+      v-for="(item, index) in navItems"
       v-bind:key="item.name + index">
       <nuxt-link v-bind:to="item.path">{{item.name}}</nuxt-link>
       </li>
@@ -11,18 +11,14 @@
 <script>
 export default {
   name: "MainNavigationList",
-  created() {
-    console.log(this.$router.options.routes);
-        this.$router.options.routes.forEach(route => {
-            this.items.push({
-                name: route.name
-                , path: route.path
-            })
-        })
-    }, data() {
-        return {
-            items: []
-        }
-    }
+  data() {
+    return {
+      navItems: [
+        { name: "Home Page", path: "/" },
+        { name: "Article", path: "/Articles" },
+        { name: "Projects", path: "/Projects" }
+      ]
+    };
+  }
 };
 </script>
