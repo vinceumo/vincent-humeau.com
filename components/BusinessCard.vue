a88195b8cbbb07ef89979725f56ae4f0c324bae7
Vincent Humeau <hello@vincent-humeau.com>
2018-10-16 17:59:01 +0100

    added vcf file download to business card

--- components/BusinessCard.vue
+++ components/BusinessCard.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="business-card">
+    <div class="business-card-content">
     <QrCodeGenerator v-bind:qrData="vCardData"/>
-    <a v-bind:href="'data:text/x-vcard;base64,' + vCardData">Add Me to your contacts</a>
+    <a class="btn is-light" v-bind:href="vCardToDataUri" download='vincent-humeau.vcf'>Download VCard</a>
+    </div>
   </div>
 </template>
 
@@ -16,6 +18,18 @@ export default {
   },
   props: {
     vCardData: String
+  },
+  computed: {
+    vCardToDataUri() {
+      const vCard = this.vCardData;
+      return (
+        "data:," +
+        encodeURIComponent(vCard)
+          .replace(/['()]/g, escape)
+          .replace(/\*/g, "%2A")
+          .replace(/%(?:7C|60|5E)/g, unescape)
+      );
+    }
   }
 };
 </script>
@@ -26,7 +40,20 @@ export default {
   color: color(dark);
   text-align: center;
   border-radius: 5px;
-  padding: spacer(2);
+  position: relative;
+  display: block;
+  height: 0;
+  padding-bottom: (85 / 55) * 100%;
+  overflow: hidden;
+
+  .business-card-content {
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    position: absolute;
+    padding: spacer(2);
+  }
 
   a {
     color: color(dark);
