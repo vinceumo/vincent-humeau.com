e7aec0b05746cf186d39a00bea4889d0484d86fd
vinceumo <vincent.humeau@gmail.com>
2020-08-10 10:12:26 +0100

    more styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import "../style/_designToken.scss"
+import "../style/style.scss"
 import "normalize.css";
 
 import Header from "./header"
